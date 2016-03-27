a56050bcd79a811978cd0de22c214bc819c6ce3a
ubershibs <luke.walker@gmail.coml>
2016-03-27 03:28:01 -0400

    Add all solutions to date

--- moderate/email_validation.rb
+++
@@ -1,6 +0,0 @@
-File.open(ARGV[0]).each_line do |line|
-#File.open("sample.txt", "r").each_line do |line|
-  re =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  puts "True" if line.chomp.match re
-  puts "False" if not line.chomp.match re
-end
